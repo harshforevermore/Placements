c68ac52c24fc5ba6f285a3de75583b28c94b0647
Raushan Kumar <92353031+raushan736834@users.noreply.github.com>
2025-06-03 18:15:04 +0530

    Delete Server directory

--- Server/src/main/java/com/CRT/College/Project/Repository/StudentEducationalDetailsRepo.java
+++
@@ -1,7 +0,0 @@
-package com.CRT.College.Project.Repository;
-
-import com.CRT.College.Project.Model.StudentEducationalDetails;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentEducationalDetailsRepo extends JpaRepository<StudentEducationalDetails,String> {
-}
--- Server/src/main/java/com/CRT/College/Project/Repository/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.CRT.College.Project.Repository;
-
-import com.CRT.College.Project.Model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<User, String> {
-    User findByRegNo(String regNo);
-    User findByCollegeEmail(String collegeEmail);
-}
